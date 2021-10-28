31da7e29ffa2831ec71f40979bab3c41df5b8129
arpitayushman <arpit.ayushman98@gmail.com>
2021-10-28 17:58:46 +0530

    Updated UI

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,12 +1,11 @@
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import "./homepage.css"
-const Homepage = () => {
+const Homepage = ({updateUser}) => {
     return(
         <div className="homepage">
             <h1>Hello you are on homepage</h1>
-            <div className="button">Logout</div>
-        </div>
+            <div className="button" onClick={() => updateUser({})} >Logout</div>        </div>
     )
 }
 export default Homepage
